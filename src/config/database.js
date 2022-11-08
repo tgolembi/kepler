1a7fe83dac53fddd0c5827b61019cdc8ec3f1c0b
Ton Golembiouski <tgolembi@gmail.com>
2022-11-08 15:35:39 -0300

    added .env features

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,17 @@
+require('dotenv').config();
+
 module.exports = {
-	dialect: 'postgres',
-	host: '',
-	username: '',
-	password: '',
-	database: '',
+	dialect: process.env.DB_DIAL,
+	host: process.env.DB_HOST,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
 	define: {
 		timestamps: true,
 		underscored: true
-	}
+	},
+	dialectOptions: {
+		useUTC: false, // reading from database
+	},
+	timezone: '-03:00', // writing to database
 };
