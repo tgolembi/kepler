6dc87b1934132dbf9e939f450649ac37713fd527
Ton Golembiouski <tgolembi@gmail.com>
2022-11-10 11:24:34 -0300

    fix ItemType fields: only alias

--- src/models/ItemType.js
+++ src/models/ItemType.js
@@ -3,16 +3,7 @@ const { Model, DataTypes } = require('sequelize');
 class ItemType extends Model {
 	static init(sequelize) {
 		super.init({
-			code: DataTypes.STRING,
-			alias: DataTypes.STRING,
-			description: DataTypes.STRING,
-			manufacturer_id: DataTypes.INTEGER,
-			type_id: DataTypes.INTEGER,
-			series: DataTypes.STRING,
-			model: DataTypes.STRING,
-			version: DataTypes.STRING,
-			active: DataTypes.BOOLEAN,
-			notes: DataTypes.STRING
+			alias: DataTypes.STRING
 		},{
 			sequelize,
 			freezeTableName: true,
